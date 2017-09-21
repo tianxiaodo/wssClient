9193b28bd6f914aad208c2c116b8fc8ddd5f1b59
田秋冬 <winter5400@sina.com>
2017-09-21 15:22:12 +0800

    set fis3 config incloud all files

--- fis-conf.js
+++ fis-conf.js
@@ -1,8 +1,7 @@
-fis.match('/page/*', {
+fis.match('**.{js,css,scss,html,jpg,png,gif,swf,tmpl}', {
   deploy: fis.plugin('http-push', {
     receiver: 'http://47.93.198.160:8999/receiver',
-    
     to: '/var/www/'
   })
-})
+});
 
